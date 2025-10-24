2b78a02991595ce697bbb61c8b477b7a7d4a775c
MustBeViable <elias1rinne@gmail.com>
2025-10-24 09:39:46 +0300

    t1 fix, t2 completed

--- ajax/t1/t1.js
+++ ajax/t1/t1.js
@@ -1,10 +1,10 @@
-const API_KEY = "M3tr0p0l14!";
+const API_KEY = "reqres-free-v1";
 
 async function test() {
   const url = "https://reqres.in/api/users/1";
   try {
     const response = await fetch(url, {
-      headers: { Authorization: `Bearer ${API_KEY}` },
+      headers: { 'x-api-key': API_KEY },
     });
     const json = await response.json();
     console.log(json);
--- ajax/t2/t2.js
+++ ajax/t2/t2.js
@@ -1,12 +1,22 @@
+const API_KEY = "reqres-free-v1";
+data = {
+  name: "morpheus",
+  job: "leader",
+};
+
 async function test() {
-    const url = 'https://reqres.in/api/users/1';
-    try {
-        const response = await fetch(url);
-        const json = await response.json();
-        console.log(json);
-    } catch (e) {
-        console.log(e.message);
-    }
+  const url = "https://reqres.in/api/users";
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "x-api-key": API_KEY, "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    const json = await response.json();
+    console.log(json);
+  } catch (e) {
+    console.log(e.message);
+  }
 }
 
 test();
