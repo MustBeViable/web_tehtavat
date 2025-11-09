5920080cb682556c4283ca4d0460014a879b5471
Elias Rinne <elias1rinne@gmail.com>
2025-11-09 09:57:12 +0200

    stuff

--- good.js
+++ good.js
@@ -1,6 +1,27 @@
-const tableRows = 4;
-const tdElements = 4;
+const table = document.getElementById("table");
 
 const questionList = [];
+const buttonHTML = `<button id="escape-button"></button>`
+
+function tableRows(table, rowCount, columnCount) {
+    for (let i = 1; i <= rowCount; i++) {
+        const tr = document.createElement("tr");
+        for (let j = 1; j <= columnCount; j++) {
+            const td = document.createElement("td");
+            td.innerText = "jeejee"
+            td.id = `${i}` + `${j}`;
+            td.classList.add("big");
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
+}
+
+function escapingButton(element) {
+    element.innerHTML = "";
+    
+}
 
 const tableMaker = () => {}
+
+tableRows(table, 4, 4);
